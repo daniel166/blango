3f08357f0f65b13206694098d82dff6a0b50d75c
Daniel Zlacký <daniel.zlacky@gmail.com>
2024-08-20 12:39:36 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -60,6 +60,7 @@ class Dev(Configuration):
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
         "rest_framework",
+        "rest_framework.authtoken",
     ]
 
     SITE_ID = 1
@@ -191,6 +192,14 @@ class Dev(Configuration):
     # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ]
+    }
 
 
 class Prod(Dev):
