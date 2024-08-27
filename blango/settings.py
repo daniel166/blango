71ecfc3067a5898bbfd3bed8965be0e3882c2da7
Daniel Zlacký <daniel.zlacky@gmail.com>
2024-08-27 08:51:44 +0000

    Finish django-versatileimagefield

--- blango/settings.py
+++ blango/settings.py
@@ -64,6 +64,7 @@ class Dev(Configuration):
         "rest_framework.authtoken",
         "drf_yasg",
         "django_filters",
+        "versatileimagefield",
     ]
 
     SITE_ID = 1
@@ -237,6 +238,9 @@ class Dev(Configuration):
         "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
         "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     }
+
+    MEDIA_ROOT = BASE_DIR / "media"
+    MEDIA_URL = "/media/"
 
 
 class Prod(Dev):
